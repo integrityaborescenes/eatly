6050613a73d525b3b658d0d8a5cbee5be0f5ce5c
Vladislav Shitov <sayroxskrim@bk.ru>
2025-10-06 20:04:22 +0400

    js slider done

--- scripts/reviewSlider.js
+++ scripts/reviewSlider.js
@@ -3,23 +3,34 @@ const reviewBlock = reviewCont.querySelector('.anotherReviewBlock')
 const sliderLineBack = reviewCont.querySelector('.sliderLineBack')
 const sliderLine = sliderLineBack.querySelector('.sliderLine')
 
-let xElem = 0;
 let xLine = 0;
-let startMove = 0;
+let startX = 0;
 let moved = false
 
 reviewCont.addEventListener('mousedown', (e) => {
+    startX =  e.clientX - xLine
+})
+
+sliderLine.addEventListener('mousedown', (e) => {
     moved = true
     sliderLine.style.cursor = 'grabbing'
-    startMove =  e.clientX
-    console.log(startMove)
 })
 
 reviewCont.addEventListener('mousemove', (e) => {
     if (moved) {
-        xLine = e.offsetX
+        let lineWidth = sliderLine.offsetWidth
+        let lineWidthBack = sliderLineBack.offsetWidth
+        xLine = e.clientX - startX
+        if (xLine < 0) {
+            xLine = 0
+        }
+        if (xLine > (lineWidthBack - lineWidth)) {
+            xLine = lineWidthBack - lineWidth
+        }
         sliderLine.style.transform = `translateX(${xLine}px)`
-        reviewBlock.style.transform = `translateX(${-xLine}px)`
+        reviewBlock.style.transform = `translateX(${-xLine*2.4}px)`
+    } else {
+        return
     }
 })
 
