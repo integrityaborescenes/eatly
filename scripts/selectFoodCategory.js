f62abfef5fdd218d2f045d60fc6cd24d1dec7513
Vladislav Shitov <sayroxskrim@bk.ru>
2025-10-11 16:16:43 +0400

    select food category fix

--- scripts/selectFoodCategory.js
+++ scripts/selectFoodCategory.js
@@ -5,39 +5,39 @@ const foodCategoryDonat = foodCategorysSelect.querySelector('.foodCategoryDonat'
 const foodCategoryIce = foodCategorysSelect.querySelector('.foodCategoryIce')
 
 let c = 0
-const whichSelected = (k) => {
+const whichSelected = (c) => {
     foodCategoryPizza.classList.remove('active')
     foodCategoryAsian.classList.remove('active')
     foodCategoryDonat.classList.remove('active')
     foodCategoryIce.classList.remove('active')
 
-    if (k===1) {
+    if (c===1) {
         foodCategoryPizza.classList.toggle('active')
-    } else if (k===2) {
+    } else if (c===2) {
         foodCategoryAsian.classList.toggle('active')
-    } else if (k===3) {
+    } else if (c===3) {
         foodCategoryDonat.classList.toggle('active')
-    } else if (k===4) {
+    } else if (c===4) {
         foodCategoryIce.classList.toggle('active')
     }
 }
 
 foodCategoryPizza.addEventListener('click', (e) => {
-    k = 1
-    whichSelected(k)
+    c = 1
+    whichSelected(c)
 })
 
 foodCategoryAsian.addEventListener('click', (e) => {
-    k = 2
-    whichSelected(k)
+    c = 2
+    whichSelected(c)
 })
 
 foodCategoryDonat.addEventListener('click', (e) => {
-    k = 3
-    whichSelected(k)
+    c = 3
+    whichSelected(c)
 })
 
 foodCategoryIce.addEventListener('click', (e) => {
-    k = 4
-    whichSelected(k)
+    c = 4
+    whichSelected(c)
 })
